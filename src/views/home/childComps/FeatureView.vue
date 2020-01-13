ed0e8fd28bc3e13935c03a13152f578ca40b152c
1836661896 <1836661896@qq.com>
2020-01-13 15:29:46 +0800

    first commit

--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -1,23 +1,10 @@
 <template>
   <div class="feature">
     <img src="~assets/img/home/recommend_bg.jpg" alt="">
-    <!-- <feature-view>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-      <feature-view-item></feature-view-item>
-    </feature-view> -->
   </div>  
 </template>
 
 <script>
-
-  // import FeatureView from 'components/common/FeatureView/FeatureView'
-  // import FeatureViewItem from 'components/common/FeatureView/FeatureViewItem'
-
 export default {
   name: 'FeatureView'
 }
