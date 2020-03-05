7f34bbb9fc67d5bf214b87d8f2bf43570db194ee
1836661896 <1836661896@qq.com>
2020-03-05 22:21:09 +0800

    first commit

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -12,6 +12,16 @@
 
   export default {
     name: 'Scroll',
+    props: {
+      probeType: {
+        type: Number,
+        default: 0
+      },
+      pullUpLoad: {
+        type: Boolean,
+        default: false
+      }
+    },
     data() {
       return {
         scroll: null
@@ -19,8 +29,36 @@
     },
     mounted() {
       this.scroll = new BScroll(this.$refs.wrapper, {
-
+        probeType: this.probeType,
+        /* 滚动监听 
+        *   默认值为0，表示不监听
+        *   1也表示不监听
+        *   2表示监听手指(鼠标)滑动时，但是离开后的惯性滚动不监听
+        *   3表示离开后的惯性滚动也监听
+        */
+        pullUpLoad: this.pullUpLoad,
+        // 上拉加载更多
+        click: true
+        // 鼠标点击事件
       })
+
+      this.scroll.on('scroll', position => {
+        // console.log(position)
+        this.$emit('scroll', position)
+      })
+
+      this.scroll.on('pullingUp', () => {
+        // console.log('上拉加载更多')
+        this.$emit('pullingup')
+      })
+    },
+    methods: {
+      scrollTo(x, y, time=300) {
+        this.scroll.scrollTo(x, y, time)
+      },
+      finishPullUp() {
+        this.scroll.finishPullUp()
+      }
     }
   }
 </script>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,117 +2,22 @@
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
     
-    <scroll class="content">
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type='3' 
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingup="loadmore">
       <home-swiper :banners='banners'/>
       <recommend-view :recommends='recommends'></recommend-view>
       <frature-view />
-      <tab-control :titles="['流行', '新款', '精选']" class="tab-control" @tabClick='tabClick'/>
+      <tab-control :titles="['流行', '新款', '精选']" 
+                   class="tab-control" 
+                   @tabClick='tabClick'/>
       <goods-list :goods="goods[currentType].list"/>
     </scroll>
 
-<ul>
-  <li>内容1</li>
-  <li>内容2</li>
-  <li>内容3</li>
-  <li>内容4</li>
-  <li>内容5</li>
-  <li>内容6</li>
-  <li>内容7</li>
-  <li>内容8</li>
-  <li>内容9</li>
-  <li>内容10</li>
-  <li>内容11</li>
-  <li>内容12</li>
-  <li>内容13</li>
-  <li>内容14</li>
-  <li>内容15</li>
-  <li>内容16</li>
-  <li>内容17</li>
-  <li>内容18</li>
-  <li>内容19</li>
-  <li>内容20</li>
-  <li>内容21</li>
-  <li>内容22</li>
-  <li>内容23</li>
-  <li>内容24</li>
-  <li>内容25</li>
-  <li>内容26</li>
-  <li>内容27</li>
-  <li>内容28</li>
-  <li>内容29</li>
-  <li>内容30</li>
-  <li>内容31</li>
-  <li>内容32</li>
-  <li>内容33</li>
-  <li>内容34</li>
-  <li>内容35</li>
-  <li>内容36</li>
-  <li>内容37</li>
-  <li>内容38</li>
-  <li>内容39</li>
-  <li>内容40</li>
-  <li>内容41</li>
-  <li>内容42</li>
-  <li>内容43</li>
-  <li>内容44</li>
-  <li>内容45</li>
-  <li>内容46</li>
-  <li>内容47</li>
-  <li>内容48</li>
-  <li>内容49</li>
-  <li>内容50</li>
-  <li>内容51</li>
-  <li>内容52</li>
-  <li>内容53</li>
-  <li>内容54</li>
-  <li>内容55</li>
-  <li>内容56</li>
-  <li>内容57</li>
-  <li>内容58</li>
-  <li>内容59</li>
-  <li>内容60</li>
-  <li>内容61</li>
-  <li>内容62</li>
-  <li>内容63</li>
-  <li>内容64</li>
-  <li>内容65</li>
-  <li>内容66</li>
-  <li>内容67</li>
-  <li>内容68</li>
-  <li>内容69</li>
-  <li>内容70</li>
-  <li>内容71</li>
-  <li>内容72</li>
-  <li>内容73</li>
-  <li>内容74</li>
-  <li>内容75</li>
-  <li>内容76</li>
-  <li>内容77</li>
-  <li>内容78</li>
-  <li>内容79</li>
-  <li>内容80</li>
-  <li>内容81</li>
-  <li>内容82</li>
-  <li>内容83</li>
-  <li>内容84</li>
-  <li>内容85</li>
-  <li>内容86</li>
-  <li>内容87</li>
-  <li>内容88</li>
-  <li>内容89</li>
-  <li>内容90</li>
-  <li>内容91</li>
-  <li>内容92</li>
-  <li>内容93</li>
-  <li>内容94</li>
-  <li>内容95</li>
-  <li>内容96</li>
-  <li>内容97</li>
-  <li>内容98</li>
-  <li>内容99</li>
-  <li>内容100</li>
-</ul>
-
+    <back-top @click.native="backClick" v-show="showBack"/>
   </div>
 </template>
 
@@ -122,9 +27,10 @@ import RecommendView from './childComps/RecommendView'
 import FratureView from './childComps/FeatureView'
 
 import NavBar from "components/common/navbar/NavBar"
-import TabControl from 'components/content/tabcontrol/TabControl'
+import TabControl from 'components/content/tabControl/TabControl'
 import GoodsList from 'components/content/goods/GoodsList'
 import Scroll from 'components/common/scroll/Scroll'
+import BackTop from 'components/content/backTop/BackTop'
 
 import { getHomeMultidata, getHomeGoods } from "network/home"
 
@@ -137,7 +43,8 @@ export default {
     NavBar,
     TabControl,
     GoodsList,
-    Scroll
+    Scroll,
+    BackTop
   },
   data() {
     return {
@@ -148,8 +55,9 @@ export default {
         new: {page: 0, list: []},
         sell: {page: 0, list: []}
       },
-      currentType: 'pop'
-    };
+      currentType: 'pop',
+      showBack: false
+    }
   },
   created() {
     // 请求多个数据
@@ -169,11 +77,20 @@ export default {
           this.currentType = 'new'
           break
         case 2:
-          this.currentType = 'sell'
-        
+          this.currentType = 'sell'        
       }
     },
-
+    backClick() {
+      this.$refs.scroll.scrollTo(0, 0)
+    },
+    contentScroll(position) {
+      // console.log(position.y)
+      this.showBack = (-position.y) > 1000
+    },
+    loadmore() {
+      this.getHomeGoods(this.currentType)
+      // console.log(this.goods[this.currentType].page)
+    },
 
     // 网络请求相关方法
     getHomeMultidata() {
@@ -188,6 +105,10 @@ export default {
         // console.log(res)
         this.goods[type].list.push(...res.data.list)
         this.goods[type].page += 1
+
+        setTimeout(() => {
+          this.$refs.scroll.finishPullUp()
+        }, 3000);
       })
     }
   }
@@ -197,6 +118,7 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
 }
 .home-nav {
   background-color: var(--color-tint);
@@ -215,7 +137,10 @@ export default {
 }
 
 .content {
-  height: 300px;
-  overflow: hidden;
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  right: 0;
+  left: 0;
 }
 </style>
